ca8f573564ab33096811de480ac6657b9cb2f97b
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-05-27 00:26:52 +0300

    add wall elements

---
+++ CourseWork/CourseWorkLib/UnitWall.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseWorkLib
+{
+    public class UnitWall : RectangleElement
+    {
+        public string color { get; }
+
+        public string code = "w";
+
+        protected int maxWidth = 3;
+        public int width { get; }
+        public int length { get; }
+
+        public int height { get; }
+
+        public UnitWall(int length, int height, int width=1) : base(width, length)
+        {
+            this.height = height;
+            base.codeElement = code;
+            if(width <= maxWidth)
+            {
+                this.width = width;
+               
+            }
+            else
+            {
+                throw new Exception("Wall must be in diapason from 1 to 2");
+            }
+        }
+
+        public void addWall(Space space, int xStart, int yStart)
+        {
+            if(space.height < this.height)
+            {
+                throw new Exception("Wall's height is more than space's height");
+            }
+            base.addElementToSpace(space, xStart, yStart);
+        }
+
+        public void removeWall(Space space)
+        {
+            base.removeElementFromSpace(space);
+        }
+
+        public void moveWallRigth(Space space, int position)
+        {
+            base.moveRight(space, position);
+        }
+
+        public void moveWallLeft(Space space, int position)
+        {
+            base.moveLeft(space, position);
+        }
+
+        public void moveWallToUp(Space space, int position)
+        {
+            base.moveUp(space, position);
+        }
+
+        public void moveWallToDown(Space space, int position)
+        {
+            base.moveDown(space, position);
+        }
+
+        public void turnWall(Space space)
+        {
+            base.turnElement(space);
+        }
+    }
+}
---
+++ CourseWork/CourseWorkLib/Wall.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseWorkLib
+{
+    public class Wall {
+
+        private bool outsideStandardWall = false;
+
+        private int wallOtsideWidth = 2;
+
+        private int wallInsideWidth = 1;
+
+        private int xStart = 1;
+        private int yStart = 1;
+        public void addOutsideWall(Space space)
+        {
+            UnitWall sideUnit = new UnitWall(space.length, space.height, wallOtsideWidth);
+            sideUnit.turnWall(space);
+            sideUnit.addWall(space, xStart, space.length - yStart);
+
+
+            int lengthMainWall = space.length - (2 * wallOtsideWidth);
+            int yForMainWall = sideUnit.width + 1;
+            int xForMainWall = space.width - wallOtsideWidth;
+            UnitWall mainUnit = new UnitWall(lengthMainWall , space.height, wallOtsideWidth);
+            mainUnit.addWall(space, xStart, yForMainWall);
+            mainUnit.moveWallToDown(space, xForMainWall);
+            mainUnit.addWall(space, xStart, yForMainWall);
+
+            outsideStandardWall = true;
+
+        }
+
+        public void addRoom(Space space, int width, int length, int x, int y)
+        {
+            if (Math.Abs(x - wallInsideWidth) <= wallInsideWidth)
+            {
+                throw new Exception("too small size of room");
+            }
+
+            UnitWall mainUnits = new UnitWall(length, space.height, wallInsideWidth);
+            mainUnits.addWall(space, x, y);
+            mainUnits.addWall(space, x + length-wallInsideWidth, y);
+
+            int newLength = length - (2 * wallInsideWidth);
+            UnitWall sideUnits = new UnitWall(newLength, space.height, wallInsideWidth);
+            sideUnits.addWall(space, x+wallInsideWidth, y);
+            sideUnits.turnWall(space);
+            sideUnits.addWall(space, x + wallInsideWidth, y + length-wallInsideWidth);
+
+        }
+
+    }
+}
