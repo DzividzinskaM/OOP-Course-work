e71aabce70472ed746e8e3f69f8fbb9894159f18
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-05-30 23:57:33 +0300

    add attributes( for adding creating element)

--- CourseWork/CourseWorkLib/Space.cs
+++ CourseWork/CourseWorkLib/Space.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CourseWorkLib.FurnitureFactory;
 
 namespace CourseWorkLib
 {
@@ -8,16 +9,28 @@ namespace CourseWorkLib
     {
         public int length { get; }
         public int width { get; }
-
         public int height { get;  }
 
         public string[,] matrixSpace;
 
         internal string emptyElemInSpace = "0";
 
+        public List<WallUnit> walls { get; internal set; }
+        public List<DoorUnit> doors;
+        public List<WindowUnit> windows;
+        public List<Bed> beds;
+        public List<Chair> chairs;
+        public List<Wardrobe> wardrobes;
+
         public Space(int width, int length, int height)
         {
-            if(length>0 && width>0 && height > 0)
+            walls = new List<WallUnit>();
+            doors = new List<DoorUnit>();
+            windows = new List<WindowUnit>();
+            beds = new List<Bed>();
+            chairs = new List<Chair>();
+            wardrobes = new List<Wardrobe>();
+            if (length>0 && width>0 && height > 0)
             {
                 this.length = length;
                 this.height = height;
