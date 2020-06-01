384c313de52fe9fd07d4f5f28b666994ebd981d1
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-06-01 21:39:39 +0300

    add class 'DesignSpaceException'

---
+++ CourseWork/CourseWorkLib/Exception/DesignSpaceException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseWorkLib.Exception
+{
+    class DesignSpaceException : System.Exception
+    {
+        public string Value { get; }
+        public DesignSpaceException() : base() { }
+        public DesignSpaceException(string message) : base(message) { }
+        public DesignSpaceException(string message, System.Exception inner) : base(message, inner) { }
+        public DesignSpaceException(string message, string val) : base(message)
+        {
+            Value = val;
+        }
+
+        protected DesignSpaceException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
